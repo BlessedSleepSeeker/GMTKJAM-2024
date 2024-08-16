73a5a1e9c9d702a7d04db62ccde1f41907def6fc
Eindall <whitedragon43000@gmail.com>
2024-08-17 00:20:56 +0200

    Added GMTK logo, transitions, credits and game name in menus

--- GMTK_Jam_2024/project/scenes/title_sequence/title_sequence.gd
+++ GMTK_Jam_2024/project/scenes/title_sequence/title_sequence.gd
@@ -1,6 +1,6 @@
 extends Setting
 
-@export var main_menu = preload("res://scenes/menu/main_menu.tscn")
+@export var gmtk_logo = preload("res://scenes/title_sequence/gmtk_logo.tscn")
 @onready var sprite: AnimatedSprite2D = $LogoAnimation
 
 signal transition(new_scene: PackedScene, animation: String)
@@ -9,9 +9,9 @@ func _ready():
 	sprite.play("logo")
 	await sprite.animation_finished
 	await get_tree().create_timer(0.5).timeout
-	transition.emit(main_menu, "")
+	transition.emit(gmtk_logo, "scene_transition")
 
 func _input(event):
 	print(event)
 	if event is InputEventKey or event is InputEventMouseButton or event is InputEventJoypadButton:
-		transition.emit(main_menu, "")
+		transition.emit(gmtk_logo, "scene_transition")
