e9e84e48978ecd19a1d7baea58624fb8fdf6a12d
Eindall <whitedragon43000@gmail.com>
2024-08-17 18:08:28 +0200

    Integrated choice buttons in game

--- GMTK_Jam_2024/project/scenes/game_scene/game.gd
+++ GMTK_Jam_2024/project/scenes/game_scene/game.gd
@@ -2,7 +2,8 @@ extends Node
 
 @onready var combat_handler: CombatHandler = null
 @onready var ui: UiViewport = find_child('UI')
-@onready var turn_counter: Label = ui.find_child('Turn_counter')
+@onready var turn_counter: Label = ui.turn_counter
+@onready var round_counter: Label = ui.round_counter
 @onready var score_counter: Label = ui.find_child('Score_counter')
 
 var current_options: Array[String] = []
@@ -10,18 +11,17 @@ var current_options: Array[String] = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	combat_handler = find_child('CombatHandler')
-	current_options = combat_handler.get_options()
 	start_turn()
 	
 func start_turn() -> void:
 	current_options = combat_handler.get_options()
+	print(combat_handler.current_round)
+	print(combat_handler.current_subround)
 	turn_counter.set_text(str(combat_handler.current_round))
+	round_counter.set_text(str(combat_handler.current_subround))
 	for option: String in current_options:
-		spawn_option(option)
+		ui.create_option(option)
 
-func spawn_option(option: String) -> void:
-	ui.create_option(option)
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- GMTK_Jam_2024/project/scenes/game_scene/ui/ui_viewport.gd
+++ GMTK_Jam_2024/project/scenes/game_scene/ui/ui_viewport.gd
@@ -2,11 +2,22 @@ extends SubViewportContainer
 class_name UiViewport
 
 @export var option_button: PackedScene
+@onready var turn_counter = $SubViewport/MarginContainer/Turns/HBoxContainer/Turn_counter
+@onready var round_counter = $SubViewport/MarginContainer/Turns/HBoxContainer2/Round_counter
+
 
 func create_option(name: String) -> void:
+	print(name)
+	var button_path = find_child('ButtonPath')
 	var option = option_button.instantiate()
-	var option_location = $SubViewport/ButtonPath/ButtonSpawnLocation
-	option_location.progress_ratio = randf()
-	option.position = option_location.position
-	option.find_child('Button').text = name
-	add_child(option)
+	option.option_name = name
+	button_path.add_child(option)
+	
+	var length = button_path.curve.get_baked_length()
+	var total_childs = button_path.get_child_count()
+	var unit = length / total_childs
+	var i = 0
+	
+	for child: PathFollow2D in button_path.get_children():
+		child.progress = unit * i
+		i += 1
